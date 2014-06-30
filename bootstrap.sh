3adb9b12dcf8f73b26aa9964ae34bc1107ff75ce
Bob VanderClay <bob@vanderclay.net>
2014-06-30 16:48:42 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -32,7 +32,7 @@
 # Homeshick, which will manage our dotfiles.
 ######################################################################
 git clone git://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
-$HOME/.homesick/repos/homeshick/bin/homeshick clone git@github.com:takitapart/dotfiles.git
+$HOME/.homesick/repos/homeshick/bin/homeshick clone https://github.com/takitapart/dotfiles.git
 
 # Open the directory. 
 cd $HOME/.homesick/repos/dotfiles
