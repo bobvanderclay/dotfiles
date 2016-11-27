1e210d9be92e153d5252910a98d62a1fce3c13dd
Bob VanderClay <bob@vanderclay.net>
2016-11-26 17:00:18 -0800

    Added git zsh hammerspoon osx roles.

---
+++ roles/zsh/aliases.zsh
@@ -0,0 +1,14 @@
+# Pipe my public key to my clipboard.
+alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
+
+# Syntax-highlighted cat (requires python-pygments)
+# alias dog="pygmentize -g"
+
+alias cl="clear"
+alias c="clear"
+alias pg='ps -ef | grep'
+alias lj='jobs'
+
+alias reload!='. ~/.zshrc'
+alias vi="vim"
+alias v="vim"
