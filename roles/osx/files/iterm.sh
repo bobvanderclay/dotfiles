4bb883acd24fc34f0872828202da8a11bebe2107
Bob VanderClay <bob@vanderclay.net>
2017-01-04 09:51:27 -0800

    Added utilities, item settings.

---
+++ roles/osx/files/iterm.sh
@@ -0,0 +1,8 @@
+# iTerm window title colors
+# Gruvbox
+echo -e "\033]6;1;bg;red;brightness;36\a"
+echo -e "\033]6;1;bg;green;brightness;36\a"
+echo -e "\033]6;1;bg;blue;brightness;36\a"
+
+# Reset
+# echo -e "\033]6;1;bg;*;default\a"
