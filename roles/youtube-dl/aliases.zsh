d67a2ed2be7decbd7ecea3d886d7a2a5f757b32c
Bob VanderClay <bob@vanderclay.net>
2017-09-23 10:06:38 -0700

    youtube-dl

---
+++ roles/youtube-dl/aliases.zsh
@@ -0,0 +1,2 @@
+alias yt-audio='youtube-dl --ignore-config -f bestaudio/best --extract-audio --audio-format m4a --audio-quality 0 --embed-thumbnail --output "~/Music/%(title)s.%(ext)s" --add-metadata'
+
