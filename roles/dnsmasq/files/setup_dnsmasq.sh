ebf09a87799cf4d4d24012634111657c1afa583e
Bob VanderClay <bob@vanderclay.com>
2019-10-31 13:16:13 -0700

    Cleanup of unused packages.

--- roles/dnsmasq/files/setup_dnsmasq.sh
+++ roles/dnsmasq/files/setup_dnsmasq.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-# Direct all *.dev domains to 127.0.0.1.
+# Direct all *.test domains to 127.0.0.1.
 mkdir -pv $(brew --prefix)/etc/
-echo 'address=/.dev/127.0.0.1' > $(brew --prefix)/etc/dnsmasq.conf
+echo 'address=/test/127.0.0.1' > $(brew --prefix)/etc/dnsmasq.conf
 sudo cp -v $(brew --prefix dnsmasq)/homebrew.mxcl.dnsmasq.plist /Library/LaunchDaemons
 sudo launchctl load -w /Library/LaunchDaemons/homebrew.mxcl.dnsmasq.plist
 sudo mkdir -v /etc/resolver
-sudo bash -c 'echo "nameserver 127.0.0.1" > /etc/resolver/dev'
+sudo bash -c 'echo "nameserver 127.0.0.1" > /etc/resolver/test'
