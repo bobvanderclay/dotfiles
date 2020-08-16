7dc440a82d95857198afd42ecc68b31169d45096
Bob VanderClay <bob@vanderclay.net>
2020-08-16 14:38:20 -0700

    Emacs gcal

--- roles/emacs/files/.doom.d/packages.el
+++ roles/emacs/files/.doom.d/packages.el
@@ -12,6 +12,7 @@
 (package! all-the-icons)
 (package! all-the-icons-dired)
 (package! default-text-scale)
+(package! org-gcal)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
